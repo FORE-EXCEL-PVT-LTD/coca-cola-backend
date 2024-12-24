9c20393b5a7cf4274e733fa8d2d5312153585de0
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2024-12-24 11:12:04 +0530

    ip

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/mongoConfig'); 
 const routes = require('./routes/routes.js');
-
+const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 5000;
-
+fetch("https://api.ipify.org?format=json")
+  .then(res => res.json())
+  .then(data => console.log("Server IP:", data.ip))
+  .catch(err => console.error("Failed to fetch IP:", err));
 app.use(cors());
 app.use(express.json()); 
 app.use('/api', routes); 
